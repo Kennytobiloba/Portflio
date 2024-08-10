b54e33569df75e401f168e5e97257859b8edd7cf
kennytobiloba <olaatunbikehinde@gmail.com>
2024-08-10 19:26:38 +0100

    Nav

--- src/Components/Navbar.jsx/Navbar.jsx
+++ src/Components/Navbar.jsx/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
 
         </div>
 
-        <div className={nav ? 'fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        <div className={nav ? ' scale-up-center fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-500' : 'fixed left-[-100%]'}>
              <h1 className='text-3xl primary-color m-4'>Kenny</h1>
              <ul className='p-8 text-2xl' >
              <li className='p-2'><a href="#about">About</a></li>
@@ -32,7 +32,7 @@ const Navbar = () => {
                         
             </ul> 
 
-
+ 
         </div>
      
       
--- src/Components/Work/Work.jsx
+++ src/Components/Work/Work.jsx
@@ -5,16 +5,39 @@ import imagethree from "../../assets/foodTwo.png"
 import imageFour from "../../assets/port.png"
 import imageFive from "../../assets/learn.png"
 import imageSix from "../../assets/capture.png"
+import imageSeven from "../../assets/capture.png"
+import imageEight from "../../assets/bgFive.png"
 
 const Work = () => {
   return (
-    <div className='max-w-[1200px] mx-auto p-5' id="work">
+    <div className='max-w-[1200px] mx-auto p-5 ' id="work">
         <div className='pb-8'>
             <p className='text-4xl mb-3 font-bold primary-color'>Work</p>
             <p className='text-gray-400'> Check out some of my recent work</p>
 
         </div>
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+        <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
+             shadow-lg shadow-[#040c16] group container rounded-md flex
+              justify-center mx-auto content-div h-[200px] bg-cover relative'>
+                <img src={imageEight} alt="" className='object-cover w-full h-full' />
+                <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
+                    <span className='text-2xl font-bold text-white tracking-wider'></span>
+                    <div className='pt-8 text-center'>
+                        <a href="https://bookrent.vercel.app/">
+                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white
+                             text-gray-700 font-bold text-lg'>Live</button>
+                        </a>
+
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+
             <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
              shadow-lg shadow-[#040c16] group container rounded-md flex-cover relative justify-center mx-auto content-div h-[200px] bg'>
               
@@ -48,6 +71,12 @@ const Work = () => {
                 </div>
 
             </div>
+           
+
+
+
+
+
 
 
             <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
@@ -66,22 +95,9 @@ const Work = () => {
                 </div>
 
             </div>
-            <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
-             shadow-lg shadow-[#040c16] group container rounded-md flex
-              justify-center mx-auto content-div h-[200px] bg-cover relative'>
-                <img src={imageFour} alt="" className='object-cover w-full h-full' />
-                <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
-                    <span className='text-2xl font-bold text-white tracking-wider'></span>
-                    <div className='pt-8 text-center'>
-                        <a href="https://agency-project-kzxv-p8tyl1ozh-kennytobiloba.vercel.app/">
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white
-                             text-gray-700 font-bold text-lg'>Live</button>
-                        </a>
+            
 
-                    </div>
-                </div>
-
-            </div>
+            
             <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
              shadow-lg shadow-[#040c16] group container rounded-md flex
               justify-center mx-auto content-div h-[200px] bg-cover relative'>
