e9e0d727c851d96fa5b0ec3d1382df5ac06c3f58
kennytobiloba <olaatunbikehinde@gmail.com>
2024-10-08 21:54:05 +0100

    portflio

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='max-w-[1200px] sm:h-[150px] p-12 flex justify-between mx-auto '>
+    <div className='max-w-[1200px] sm:h-[150px] p-12 flex sm:justify-between mx-auto '>
         <span className='primary-color'>Kenny</span>
         <p className='text-gray-600'>olaatunbikehinde@gmail.com</p> 
         <p className='text-gray-600'> #FrontentDev</p>
